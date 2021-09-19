2f4eab712c2ff1edfe8109342351247227ad47d2
stevesdodd <stevesdodd@users.noreply.github.com>
2021-09-19 09:26:37 +0000

    deploy: 089fea016d5c1f65013cd2bbece75c863126f623

--- game1/index.js
+++
@@ -1,9 +0,0 @@
-const draw = () => {
-    const canvas = document.getElementById("gameCanvas");
-    const ctx = canvas.getContext("2d");
-    ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx === null || ctx === void 0 ? void 0 : ctx.fillRect(0, 0, 50, 50);
-    console.log('game1');
-};
-export default draw;
-draw();
