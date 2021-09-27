17344e55eef3c24355a96c5ed5ba1d1d33dd9bba
stevesdodd <stevesdodd@users.noreply.github.com>
2021-09-27 14:54:29 +0000

    deploy: 7dc75aaea46c213a10449c5dcaf73c0c8e59f7b0

--- canvas-operations/index.js
+++ canvas-operations/index.js
@@ -1,39 +1,46 @@
 "use strict";
-const canvas = document.getElementById("gameCanvas");
-const context = canvas.getContext("2d");
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
 //Set constants
 canvas.width = 800;
 canvas.height = 3 * canvas.width / 4.0;
-const carPosX = 200;
-const baseHeight = 540;
-const roofHeight = baseHeight - 150;
-const BonnetHeight = baseHeight - 75;
-const carWidth = 450;
-const wheel1CenterX = carPosX + 75;
-const wheel1CenterY = baseHeight - 15;
-const wheel2CenterX = carPosX + carWidth - 75;
-const wheel2CenterY = baseHeight - 15;
-let image = new Image();
+var carPosX = 200;
+var baseHeight = 540;
+var roofHeight = baseHeight - 150;
+var BonnetHeight = baseHeight - 75;
+var carWidth = 450;
+var wheel1CenterX = carPosX + 75;
+var wheel1CenterY = baseHeight - 15;
+var wheel2CenterX = carPosX + carWidth - 75;
+var wheel2CenterY = baseHeight - 15;
+var image = new Image();
 image.src = "cloud-large.png";
-const eraseCanvas = (context) => {
+var eraseCanvas = function (context) {
     context.clearRect(0, 0, canvas.width, canvas.height);
 };
-const drawGradientBackground = (context) => {
-    let gradient = context.createLinearGradient(0, 0, 0, canvas.height);
+var drawGradientBackground = function (context) {
+    var gradient = context.createLinearGradient(0, 0, 0, canvas.height);
     gradient.addColorStop(0, 'lightBlue');
     gradient.addColorStop(.5, 'blue');
     gradient.addColorStop(1, 'lightBlue');
     context.fillStyle = gradient;
     context.fillRect(0, 0, canvas.width, canvas.height);
 };
-const drawFillCircle = (context, x, y, radius, color) => {
+var drawFillCircle = function (context, x, y, radius, color) {
     context.beginPath();
     context.fillStyle = color;
     context.arc(x, y, radius, 0, Math.PI * 2);
     context.fill();
     context.closePath();
 };
-const drawRoad = (context) => {
+var drawStrokeCircle = function (context, x, y, radius, color) {
+    context.beginPath();
+    context.strokeStyle = color;
+    context.arc(x, y, radius, 0, Math.PI * 2);
+    context.stroke();
+    context.closePath();
+};
+var drawRoad = function (context) {
     context.fillStyle = 'gray';
     context.fillRect(0, canvas.height - 50, canvas.width, 50);
     context.beginPath();
@@ -45,7 +52,7 @@ const drawRoad = (context) => {
     context.stroke();
     context.setLineDash([]);
 };
-const drawRoof = (context, color, delta) => {
+var drawRoof = function (context, color, delta) {
     context.fillStyle = color;
     context.beginPath();
     context.moveTo(carPosX + 25 + delta, BonnetHeight);
@@ -54,7 +61,7 @@ const drawRoof = (context, color, delta) => {
     context.lineTo(carPosX + carWidth - 75 - delta, BonnetHeight);
     context.fill();
 };
-const drawClouds = (context) => {
+var drawClouds = function (context) {
     context.save();
     context.translate(canvas.width, 0);
     context.scale(-1, 1);
@@ -63,8 +70,8 @@ const drawClouds = (context) => {
     context.drawImage(image, 450, 125, 96, 96);
     context.drawImage(image, 600, 175, 128, 128);
 };
-const drawWindowGap = (context) => {
-    const windowGapCenterX = carPosX + carWidth / 2 - 15;
+var drawWindowGap = function (context) {
+    var windowGapCenterX = carPosX + carWidth / 2 - 15;
     context.fillStyle = 'red';
     context.beginPath();
     context.moveTo(windowGapCenterX - 5, roofHeight);
@@ -74,15 +81,17 @@ const drawWindowGap = (context) => {
     context.closePath();
     context.fill();
 };
-const draw = (context) => {
+var draw = function (context) {
     drawGradientBackground(context);
     drawRoad(context);
     context.fillStyle = 'red';
     context.fillRect(carPosX, BonnetHeight, carWidth, baseHeight - BonnetHeight);
     drawFillCircle(context, wheel1CenterX, wheel1CenterY, 50, 'black');
     drawFillCircle(context, wheel1CenterX, wheel1CenterY, 40, 'gray');
+    drawStrokeCircle(context, wheel1CenterX, wheel1CenterY, 5, 'black');
     drawFillCircle(context, wheel2CenterX, wheel2CenterY, 50, 'black');
     drawFillCircle(context, wheel2CenterX, wheel2CenterY, 40, 'gray');
+    drawStrokeCircle(context, wheel2CenterX, wheel2CenterY, 5, 'black');
     drawRoof(context, 'red', 0);
     drawRoof(context, '#99f6ff', 15);
     drawWindowGap(context);
