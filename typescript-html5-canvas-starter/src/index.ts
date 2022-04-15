66811dd212143c45bda83f8ef605d8c4aa4aa9a1
stephen-dodd <stephen.dodd@rea-group.com>
2022-04-15 17:42:40 +1000

    Add keyboard input events.

--- typescript-html5-canvas-starter/src/index.ts
+++ typescript-html5-canvas-starter/src/index.ts
@@ -1,4 +1,4 @@
 const canvas: HTMLCanvasElement  = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const context = canvas.getContext("2d")
-canvas.width = 1024
+canvas.width = 800
 canvas.height = 3 * canvas.width / 4
