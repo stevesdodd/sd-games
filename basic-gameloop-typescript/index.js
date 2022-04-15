8eba21ca09b901f7f684bdacd23b7ece3a2a8120
stevesdodd <stevesdodd@users.noreply.github.com>
2022-04-15 13:03:26 +0000

    deploy: e8a54ef6e3e474d0355131faa9094ef9f227b97d

---
+++ basic-gameloop-typescript/index.js
@@ -0,0 +1,11 @@
+import GameRuntime from './GameRuntime.js';
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 3 * canvas.width / 4;
+window.onload = function () {
+    if (context) {
+        const loopTimeout = 1000 / 60;
+        setInterval(GameRuntime.loop, loopTimeout, context, canvas.width, canvas.height);
+    }
+};
