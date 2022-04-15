ad2c3f6ccd82bb77e68b78c9419feb9659c51cd3
stephen-dodd <stephen.dodd@rea-group.com>
2022-04-15 23:00:16 +1000

    Add basic gameloop typescript.

--- basic-gameloop-typescript/src/index.ts
+++ basic-gameloop-typescript/src/index.ts
@@ -2,7 +2,7 @@ import GameRuntime from './GameRuntime.js'
 
 const canvas: HTMLCanvasElement  = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const context = canvas.getContext("2d")
-canvas.width = 1024
+canvas.width = 800
 canvas.height = 3 * canvas.width / 4
 
 window.onload = function() {
