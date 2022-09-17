fb05f00698a5b41c6bf4f4bf7aca0a13c09352f2
stephen-dodd <stephen.dodd@rea-group.com>
2022-09-17 22:01:10 +1000

    Add multiplayer input example.

---
+++ multiplayer-javascript-game-input/src/types/types.ts
@@ -0,0 +1,33 @@
+import { PlayerInputs, StandardGameInputFourPlayer } from "../input/types"
+
+export type Vector2DType = {
+  x: number,
+  y: number
+}
+
+export type DirectionState = -1 | 0 | 1
+
+export type DirectionsState = {
+  horizontal: DirectionState,
+  vertical: DirectionState
+}
+
+export type PlayerState = {
+  position: Vector2DType,
+  direction: DirectionsState,
+  top: boolean,
+  right: boolean,
+  bottom: boolean,
+  left: boolean
+}
+
+export type GameState = {
+  player1: PlayerState,
+  player2: PlayerState,
+  player3: PlayerState,
+  player4: PlayerState,
+  inputs: {
+    standardGameInputFourPlayer: StandardGameInputFourPlayer,
+    playerInputMappings: PlayerInputs
+  }
+}
