dcf4050e0d7df6938f8abe93833221bfc1963b9c
stephen-dodd <stephen.dodd@rea-group.com>
2023-03-30 01:29:22 +1100

    Add variable game loop

---
+++ variable-delta-time-javascript-game-loop/src/types/types.ts
@@ -0,0 +1,51 @@
+import { PlayerInputs, StandardGameInputFourPlayer } from "../input/types.js"
+
+export type Vector2DType = {
+  x: number,
+  y: number
+}
+
+export type DirectionState = -1 | 0 | 1
+
+export type DirectionsState = {
+  horizontal: DirectionState,
+  vertical: DirectionState
+}
+
+export type PlayerState = {
+  position: Vector2DType,
+  direction: DirectionsState,
+  top: boolean,
+  right: boolean,
+  bottom: boolean,
+  left: boolean
+}
+
+export type GameState = {
+  player1: PlayerState,
+  player2: PlayerState,
+  player3: PlayerState,
+  player4: PlayerState,
+  inputs: {
+    standardGameInputFourPlayer: StandardGameInputFourPlayer,
+    playerInputMappings: PlayerInputs
+  },
+  squares: {
+    square1: {
+      pos: Vector2DType,
+      velocity: Vector2DType
+    },
+    square2: {
+      pos: Vector2DType,
+      velocity: Vector2DType
+    } 
+  },
+  engine1: {
+    timePreviousFrame: number,
+    startTime: number
+  },
+  engine2: {
+    timePreviousFrame: number,
+    startTime: number
+  }
+}
---
+++ variable-delta-time-javascript-game-loop/src/web-worker.ts
@@ -0,0 +1,3 @@
+setInterval(() => {
+  postMessage('test');
+}, 1000 / 60.0);
